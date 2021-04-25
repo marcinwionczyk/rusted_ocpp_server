3e6c9654438f279de899fde164eeba3ccce7d0ae
Marcin Wionczyk <marcin.wionczyk@gmail.com>
2021-04-25 11:37:01 +0200

    using dotenv to read settings from settings.env

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use actix_files::Files;
 use actix_web::{http, web, App, Error, HttpResponse, HttpServer};
 use awmp::Parts;
 use std::collections::HashMap;
-use std::env;
+use dotenv;
 use diesel::prelude::*;
 use diesel::mysql::MysqlConnection;
 use diesel::r2d2::{self, ConnectionManager};
@@ -28,7 +28,7 @@ struct IndexTemplateData {
 }
 
 async fn index(hb: web::Data<Handlebars<'_>>, pool: web::Data<DbPool>)
-    -> Result<HttpResponse, Error> {
+               -> Result<HttpResponse, Error> {
     let connection = pool.get().expect("Can't get db connection from pool");
     let available_chargers_data = web::block(move ||
         available_chargers.load::<AvailableCharger>(&connection))
@@ -45,22 +45,25 @@ async fn index(hb: web::Data<Handlebars<'_>>, pool: web::Data<DbPool>)
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-        let mut handlebars = Handlebars::new();
-        handlebars
-            .register_templates_directory(".html", "./static/").unwrap();
-        let handlebars_ref = web::Data::new(handlebars);
-        let database_url = env::var("DATABASE_URL")
-            .expect("DATABASE_URL must be set");
-        let manager = ConnectionManager::<MysqlConnection>::new(&database_url);
-        let pool = r2d2::Pool::builder()
-            .build(manager)
-            .expect("Failed to create DB connection pool.");
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".html", "./static/").unwrap();
+    let handlebars_ref = web::Data::new(handlebars);
+    dotenv::from_filename("settings.env").ok();
+    let env_database_url = dotenv::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    let env_ip_address = dotenv::var("IP_ADDRESS").expect("IP_ADDRESS must be set");
+    let env_port = dotenv::var("PORT").expect("PORT must be set");
+    let manager = ConnectionManager::<MysqlConnection>::new(&env_database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create DB connection pool.");
 
-        println!("server is listening on port 8080");
-        HttpServer::new(move || {
-            App::new().app_data(handlebars_ref.clone())
-                .data(pool.clone())
-                .service(Files::new("/static", "static").show_files_listing())
-                .route("/", web::get().to(index))
-        }).bind("127.0.0.1:8080")?.run().await
+    println!("server is listening on ip address {} and port {}", &env_ip_address, &env_port);
+    HttpServer::new(move || {
+        App::new().app_data(handlebars_ref.clone())
+            .data(pool.clone())
+            .service(Files::new("/static", "static").show_files_listing())
+            .route("/", web::get().to(index))
+    }).bind(format!("{}:{}", &env_ip_address, &env_port))?.run().await
 }
