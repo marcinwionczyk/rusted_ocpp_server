c407030910fbaaff76a54721c0f7e7c7aed0c2e1
Marcin Wionczyk <marcin.wionczyk@gmail.com>
2021-06-07 17:36:49 +0200

    added webclient, modified main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::time::Instant;
-
+use actix_files::Files;
 use actix::{Actor, Addr};
 use actix_web::{App, Error as ActixWebError, get, HttpRequest, HttpResponse, HttpServer, web, Responder};
 use actix_web_actors::ws;
@@ -14,7 +14,6 @@ mod client;
 mod error;
 
 const ALLOWED_SUB_PROTOCOLS: [&'static str; 1] = ["ocpp2.0.1"];
-
 
 #[get("/ocpp/{serial_id}")]
 async fn ws_index(r: HttpRequest, stream: web::Payload, srv: web::Data<Addr<server::OcppServer>>) -> Result<HttpResponse, ActixWebError> {
@@ -32,28 +31,6 @@ async fn ws_index(r: HttpRequest, stream: web::Payload, srv: web::Data<Addr<serv
     }
 }
 
-#[get("/")]
-async fn index(r: HttpRequest, srv: web::Data<Addr<server::OcppServer>>) -> Result<HttpResponse, ActixWebError> {
-    let q = r.query_string();
-    if q.len() > 0 {
-        println!("query string: {}", r.query_string());
-        let qs = QString::from(q);
-        let charge_point_name = qs.get("charge_point");
-        let choice_name = qs.get("choice");
-        if charge_point_name.is_some() && choice_name.is_some() {
-            srv.do_send(WebClientMessage {
-                serial_id: charge_point_name.unwrap().to_string(),
-                text: "".to_string(),
-            });
-        }
-        Ok(HttpResponse::Ok().body(r#"<h1>Content passed to ocpp server</h1><p><a href="/">Go back</a></p>"#))
-    } else {
-        Ok(
-            HttpResponse::Ok()
-                .content_type("text/html; charset=utf-8")
-                .body(include_str!("../static/index-old.html")))
-    }
-}
 
 #[get("/api/get-chargers")]
 async fn get_chargers(srv: web::Data<Addr<server::OcppServer>>) -> Result<impl Responder, error::Error> {
@@ -77,9 +54,9 @@ async fn main() -> std::io::Result<()> {
             .data(server.clone())
             //.data(pool.clone())
             //.service(web::resource("/").route(web::get().to(index)))
-            .service(index)
             .service(get_chargers)
             .service(ws_index)
+            .service(Files::new("/", "./webclient/").index_file("index.html"))
     })
         .bind(format!("{}:{}", config.server.host, config.server.port))?
         .run()
